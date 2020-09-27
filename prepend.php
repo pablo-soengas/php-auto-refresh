3c546022142492cba19bcfe4f165d39a9b304f4c
pablo-soengas <pabloezequiel844@gmail.com>
2020-09-27 11:34:23 -0300

    Add code to prevent executing php auto refresh while using wp-cli (command line interface for wordpress)

--- prepend.php
+++ prepend.php
@@ -7,21 +7,27 @@
  * @todo Create php file with all settings (files to refresh, directories to monitor for each file, whether to show checkbox permanently or on mouse over, or hide it, etc)
  */
 
-    //Registers a function to be executed after the execution of the main script has finished
-    register_shutdown_function(
-    /**
-     * Function to be executed after $current_file execution finishes
-     *
-     * It will output a script tag at the end of the result of the execution of $current_file, in order to call the javascript file php_ar.js.     *
-     * We pass a callback instead of a function name, in order to avoid polluting the global namespace
-     *
-     *
-     * @param string $current_file the file that the current request is for
-     * @param bool $enabled whether an entry for $current_file was found in settings.json and also the key named "enabled" in that entry has a value of "true", which means that $current_file is enabled for auto refresh in settings.json.     *
-     * @param $refresh_url
-     * @return void
-     */
-        function () {
+
+//Registers a function to be executed after the execution of the main script has finished
+register_shutdown_function(
+/**
+ * Function to be executed after $current_file execution finishes
+ *
+ * It will output a script tag at the end of the result of the execution of $current_file, in order to call the javascript file php_ar.js.     *
+ * We pass a callback instead of a function name, in order to avoid polluting the global namespace
+ *
+ *
+ * @param string $current_file the file that the current request is for
+ * @param bool $enabled whether an entry for $current_file was found in settings.json and also the key named "enabled" in that entry has a value of "true", which means that $current_file is enabled for auto refresh in settings.json.     *
+ * @param $refresh_url
+ * @return void
+ */
+    function () {
+
+        // If the user is running wp-cli, this functionality must not be executed at all
+        if (defined('WP_CLI') && WP_CLI)
+            return;
+
         //Obtains the full path to the directory where php auto refresh files were installed
         $refresh_dir = dirname(ini_get('auto_prepend_file'));
         // Obtains the name of the directory where php auto refresh files were installed
@@ -42,27 +48,27 @@
         //parse the json file with the settings into an associative array
         $settings = json_decode($settings, true);
         //iterates through the settings array to search for $current_file
-        foreach($settings['items'] as $item){
-            if($item['file'] == $current_file){
+        foreach ($settings['items'] as $item) {
+            if ($item['file'] == $current_file) {
                 //$current_file is in settings.json
                 $file_in = true;
                 break;
             }
         }
         //if $current_file was found in settings.json, check if it's enabled
-        if($file_in && $item['enabled']){
+        if ($file_in && $item['enabled']) {
             $enabled = true;
-        }else{
+        } else {
             $enabled = false;
         }
 
         $script_tag = '<script type="text/javascript" src="' . $refresh_url . '/php_ar.js" data-currentFile="' . $current_file . '" ';
         //if the file is already enabled for auto refresh in settings.json, add the data-enabled custom attribute to the script tag, so that the javascript called by this script tag knows that
         if (isset($enabled) && $enabled)
-            $script_tag .=  ' data-enabled ';
+            $script_tag .= ' data-enabled ';
         $script_tag .= 'data-refreshUrl="' . $refresh_url . '" ';
         $script_tag .= '></script>';
         echo $script_tag;
-    } );
+    });
 
 
